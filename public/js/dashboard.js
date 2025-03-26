cfe280670a0340ea1e7ca56e9afa7707ad72950c
gabriel-shanahan <41175126+gshanahan@users.noreply.github.com>
2025-03-26 16:54:47 -0400

    import errors

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,4 +1,4 @@
-  import { firebaseConfig } from "./firebaseConfig.js";
+  import { auth } from "./firebaseConfig.js";
   import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth.js";
 
   // Example sign-out function (replace with Firebase auth method)
