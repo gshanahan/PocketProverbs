c762e02cf0e9ef9b63777d7bb21017157c55df47
gabriel-shanahan <41175126+gshanahan@users.noreply.github.com>
2025-03-26 14:51:37 -0400

    ditto

--- public/js/firebaseConfig.js
+++ public/js/firebaseConfig.js
@@ -1,6 +1,6 @@
 // Import necessary Firebase modules (DO NOT use 'default')
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-auth.js";
 import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
 
 // Your Firebase configuration from the Firebase Console
