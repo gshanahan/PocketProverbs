a90890bc204512890905dc645c54c3399faa4b3a
gabriel-shanahan <41175126+gshanahan@users.noreply.github.com>
2025-03-29 13:56:55 -0400

    now it works?

--- public/js/login.js
+++ public/js/login.js
@@ -37,9 +37,6 @@ import { auth, db, doc, setDoc, getDoc, addDoc, collection, createUserWithEmailA
 
             // Save user data to Firestore
             saveUserData(userCredential.user, email);
-    
-            // Redirect after Firestore data is saved
-            //window.location.href = "/index.html";         /////ADD THIS BACK AFTER TESTING
             hideLoading();
         })
         .catch((error) => {
@@ -62,7 +59,6 @@ import { auth, db, doc, setDoc, getDoc, addDoc, collection, createUserWithEmailA
             console.log("User data saved in Firestore.");
         } catch (error) {
             console.error("Error saving user data:", error);
-            alert("Error saving user data:", error);
         }
     }
 
