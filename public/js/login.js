afe30ee2286085de96c8d346c59bd6ffaf881368
gabriel-shanahan <41175126+gshanahan@users.noreply.github.com>
2025-03-29 12:54:12 -0400

    ditto

--- public/js/login.js
+++ public/js/login.js
@@ -36,7 +36,7 @@ import { auth, db, doc, setDoc, getDoc, addDoc, collection, createUserWithEmailA
             console.log("Account created:", userCredential.user);
 
             // Save user data to Firestore
-            saveUserData(user.uid, email);
+            saveUserData(userCredential.user.uid, email);
     
             // Redirect after Firestore data is saved
             window.location.href = "/index.html";
